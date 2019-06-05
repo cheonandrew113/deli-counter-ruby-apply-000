1fc1ae395168177e2f84dd65f8a428965791b49b
Andnrew Cheon <cheonandrew113@gmail.com>
2019-06-05 18:35:21 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,4 @@
 katz_deli = []
-other_deli = ["Logan", "Avi", "Spencer"]
-another_deli = ["Amanda", "Annette", "Ruchi", "Jason", "Logan", "Spencer", "Avi", "Joe", "Rachel", "Lindsey"]
 
 def line(array)
   if array.length < 1
